2d79834015de786d972b61b7474ecc929d1f10c6
mrazomej <manuel.razo.m@gmail.com>
2019-08-23 12:54:50 -0700

    updating software module and other stuff

---
+++ rnaseq_barcode/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top level package for evo_utils utilities"""
+
+from . import viz
+
+__author__ = """Manuel Razo"""
+__email__ = """mrazomej {at} caltech.edu"""
+__version__ = '0.0.1'
+name = 'rnaseq_barcode'
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rnaseq_barcode",
+    version="0.0.1",
+    author="Manuel Razo, Niko McCarty, Rob Phillips",
+    author_email="mrazomej {at} caltech {dot} edu",
+    description="This repository contains all active research materials for the project testing the use of RNA-seq as a tool for biophysics",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mrazomej/rnaseq_barcode.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
