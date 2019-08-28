b4498ee9705330f9aa48be994e5963d5a041380f
mrazomej <manuel.razo.m@gmail.com>
2019-08-27 17:01:18 -0700

    relevant files to demultiplex sequencing runs

---
+++ code/processing/20190821_operator_library_mapping/20190821_demultiplex_seq.py
@@ -0,0 +1,137 @@
+#%%
+import os
+import itertools
+import subprocess
+import glob
+
+#%% 
+# Date for sequencing run
+DATE = 20190821
+
+# Conda environment name where qiime2 is installed
+CONDA_ENV = 'qiime2'
+
+# Path containing all raw input information
+INPUT_DIR = '../../../data/raw_sequencing/' +\
+            '20190821_operator_library_mapping/'
+
+# Path containing raw input sequences
+RAW_DIR = INPUT_DIR + 'miseq_output/Data/Intensities/BaseCalls/'
+
+# Define requirements for qiime2 demultiplexing
+# Path to barcode TSV file
+BARCODE_LIST = INPUT_DIR + 'sequencing_barcodes_qiime2.tsv'
+
+# Path to output folder
+OUTPUT_DIR = '../../../data/demux_sequencing/' +\
+             f'{DATE}_operator_library_mapping/'
+# Define temporary directory
+TMP_DIR = OUTPUT_DIR + 'tmp/'
+# Define diretory to save all of qiime2 files
+QIIME2_DIR = OUTPUT_DIR + 'qiime2_output/'
+
+# Generate output directory if it doesn't exist
+if not os.path.exists(OUTPUT_DIR):
+    os.makedirs(OUTPUT_DIR)
+
+# Generate tmp directory if it doesn't exist
+if not os.path.exists(TMP_DIR):
+    os.makedirs(TMP_DIR)
+
+# Generate tmp directory if it doesn't exist
+if not os.path.exists(QIIME2_DIR):
+    os.makedirs(QIIME2_DIR)
+
+#%%
+
+# Pattern on each of the fastq files
+FORWARD_STR = 'Undetermined*R1*fastq.gz'
+REVERSE_STR = 'Undetermined*R2*fastq.gz'
+BARCODE_STR = 'Undetermined*I1*fastq.gz'
+# Path to each file
+FORWARD_SEQ = glob.glob(RAW_DIR + FORWARD_STR)[0]
+REVERSE_SEQ = glob.glob(RAW_DIR + REVERSE_STR)[0]
+BARCODE_SEQ = glob.glob(RAW_DIR + BARCODE_STR)[0]
+
+# Define requirements for qiime2
+sequence_type = 'EMPPairedEndSequences'  # Paired end illumina sesquences
+artifact_file = f'{QIIME2_DIR}{DATE}_sequence_artifact.qza'  # output artifact
+
+# Check if this process hasn't been done before
+print('checking if qiime2 sequence artifact has been generated before.')
+if not os.path.exists(artifact_file):
+    # Temporarily rename files so that qiime2 can recognize them
+    print('temporarily moving fastq files...')
+    os.rename(FORWARD_SEQ, TMP_DIR + 'forward.fastq.gz')
+    os.rename(REVERSE_SEQ, TMP_DIR + 'reverse.fastq.gz')
+    os.rename(BARCODE_SEQ, TMP_DIR + 'barcodes.fastq.gz')
+
+    # Define bash command to generate artifact for qiime to register seqeunces
+    bash_artifact = f'''qiime tools import \
+    --type {sequence_type} \
+    --input-path  {TMP_DIR} \
+    --output-path {artifact_file}
+    '''
+
+    # Run subprocess activating qiime2 conda environment
+    print(f'generating qiime2 artifact on conda {CONDA_ENV} environment.')
+    subprocess.run(f'source activate {CONDA_ENV} && {bash_artifact}',
+                   shell=True)
+
+    # Rename files back to original name
+    print('renaming fastq files back to original name...')
+    os.rename(TMP_DIR + 'forward.fastq.gz', FORWARD_SEQ)
+    os.rename(TMP_DIR + 'reverse.fastq.gz', REVERSE_SEQ)
+    os.rename(TMP_DIR + 'barcodes.fastq.gz', BARCODE_SEQ)
+    print('DONE!')
+
+#%%
+# Define requirements for qiime2 demultiplexing
+# Column in metadata containing barcodes
+barcode_col = 'barcode-sequence'
+# File to save the demultiplexed qiime2 artifact
+demux_file = f'{QIIME2_DIR}{DATE}_demux_artifact.qza'
+# File to save the details for the demultiplexed sequences
+demux_details = f'{QIIME2_DIR}{DATE}_demux_details.qza'
+
+# Define shell command to demultiplex sequences
+# NOTE: the --p-no-golay-error-correction option is added because we have 8nt
+# barcodes rather than 12nt barcodes, so the so-called Golay correction 
+# cannot be applied
+bash_demux = f'''qiime demux emp-paired \
+    --m-barcodes-file {BARCODE_LIST} \
+    --m-barcodes-column {barcode_col} \
+    --i-seqs {artifact_file} \
+    --o-per-sample-sequences {demux_file} \
+    --o-error-correction-details {demux_details} \
+    --p-no-golay-error-correction
+  '''
+
+# Check if this process hasn't been done before
+print('checking if sequences have been demultiplexed already.')
+if not os.path.exists(demux_file):
+    # Run subprocess activating qiime2 conda environment
+    print(f'demultiplexing sequences.')
+    subprocess.run(f'source activate {CONDA_ENV} && {bash_demux}', shell=True)
+    print('DONE!')
+#%%
+# Path to save interactive plots output for the analysis
+demux_viz = f'{QIIME2_DIR}{DATE}_demix_viz.qzv'  
+
+# bash commnand to generate interactive plots and export sequences
+bash_viz = f'''qiime demux summarize \
+  --i-data {demux_file} \
+  --o-visualization {demux_viz} \
+'''
+bash_exp = f'''qiime tools export \
+  --input-path {demux_file} \
+  --output-path {OUTPUT_DIR} 
+'''
+# Check if this process hasn't been done before
+print('checking if data has already been exported.')
+if not os.path.exists(demux_viz):
+    print(f'generating interactive plots and exporting data.')
+    subprocess.run(f'source activate {CONDA_ENV} && {bash_viz} && {bash_exp}',
+                   shell=True)
+
+print('DONE!')
---
+++ code/processing/20190821_operator_library_mapping/sequencing_README.md
@@ -0,0 +1,37 @@
+---
+status: Rejected
+reason: Experiment not yet completed 
+---
+
+# 2019-08-21 Sequencing run
+
+## Purpose
+Mapping the O1, O2 and O3 oprator library barcodes to the corresponding set 
+of random barcodes.
+
+## Platform
+MiSeq system (Thomson lab)
+
+## Sequencing kit
+MiSeq nano kit V2. (300 cycles)
+
+## Sequencing Modality
+Paired-ends reads
+
+## Materials
+
+| **id** | **barcode-sequence** | **description** |
+| :--: | :--: | :--: |
+| index-1 | CAGTACAT | Technical replica for sequencing |
+| index-2 | ACTAGCAT | Technical replica for sequencing |
+
+## Notes and Observations
+This is our very first sequencing run. We were helped by Jeff Park from the 
+Thomson lab. He taught us how to operate the MiSeq and how to load the samples
+into the machine
+
+
+## Library Preparation
+
+1. Example text 1
+2. Example text 2
