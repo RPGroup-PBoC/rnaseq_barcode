67fe9025e694db406abf2068d98966ed31beb289
mrazomej <manuel.razo.m@gmail.com>
2019-10-31 12:41:29 -0700

    update plots

--- code/processing/20191030_O2_IPTG_titration_flow/analysis_flow.py
+++ code/processing/20191030_O2_IPTG_titration_flow/analysis_flow.py
@@ -46,7 +46,7 @@ theory = rnaseq.thermo.SimpleRepression(rr, ee, effector_conc=ii,
                                         ka=constants['Ka'], ki=constants['Ki'],
                                         ep_ai=constants['ep_AI'])
 
-# Compute fold-change
+# Compute fold-change for IPTG titration
 fc_theory = theory.fold_change().T
 #%%
 # Instantiate the figure canvas
@@ -78,7 +78,51 @@ _ = ax.legend(loc='upper left', title='rep./cell', fontsize=6)
 ax.set_ylim([-0.1, 1.2])
 # Save the figure.
 plt.tight_layout()
-plt.savefig('output/fold_change_curve.png')
+plt.savefig('output/fold_change_IPTG.png')
 
 
 # %%
+
+# Define repressor array
+rep_array = np.logspace(1, np.log10(2000), 50)
+# Instantiate simple repression class
+theory = rnaseq.thermo.SimpleRepression(rep_array, era, effector_conc=0, 
+                                        ka=constants['Ka'], ki=constants['Ki'],
+                                        ep_ai=constants['ep_AI'])
+
+
+# Compute fold-change for lacI titration
+fc_theory = theory.fold_change().T
+
+#%%
+
+# Define colors
+colors = sns.color_palette('Paired', n_colors=2)
+# Instantiate the figure canvas
+fig, ax = plt.subplots(1, 1)
+
+# Add labels and scaling
+ax.set_xlabel('rep./cell')
+ax.set_ylabel('fold-change')
+ax.set_xscale('log')
+ax.set_yscale('log')
+
+# Group the data by operator
+# Remove auto and delta.
+fc = data.loc[(data['strain'] != 'auto') & 
+              (data['strain'] != 'delta') &
+              (data['IPTGuM'] == 0)]
+
+# Plot the inensity curves.
+# Plot theoretical fold-change
+_ = ax.plot(rep_array, fc_theory, '-',
+            color=colors[0], label='')
+# Plot data
+_ = ax.plot(fc['repressors'], fc['fold_change'], 'o',
+            color=colors[1], markersize=5)
+
+# Add a legend.
+# Save the figure.
+plt.tight_layout()
+plt.savefig('output/fold_change_lacI.png')
+
