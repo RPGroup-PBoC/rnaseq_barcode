423b07623ebda586f2d4fe0503f90aaa90dfa5ea
mrazomej <manuel.razo.m@gmail.com>
2021-07-22 17:59:12 -0700

    working on 20210716 sequencing data

---
+++ code/processing/seq/20210716_RNA_DNA_seq/processing_seq.py
@@ -0,0 +1,60 @@
+#%%
+import os
+import glob
+import pandas as pd
+import git
+
+#%%
+# Date for sequencing run of the library to map
+DATE = 20210716
+# Description to be attached to folder names
+DESCRIPTION = '_RNA_DNA_seq/'
+
+# Find project parental folder
+repo = git.Repo('./', search_parent_directories=True)
+homedir = repo.working_dir
+
+# Path to input folder
+INPUT_DIR = f'{homedir}/data/demux_sequencing/{DATE}{DESCRIPTION}'
+
+# Path to output folder
+OUTPUT_DIR = f'{homedir}/data/processed_sequencing/{DATE}{DESCRIPTION}'
+
+# Generate output directory if it doesn't exist
+if not os.path.exists(OUTPUT_DIR):
+    os.makedirs(OUTPUT_DIR)
+
+#%%
+# Read list of demultiplexed sequences
+seq_table = pd.read_csv(INPUT_DIR + 'MANIFEST', comment="#")
+
+# Group sequences by index to feed them into the fastp
+seq_group = seq_table.groupby('sample-id')
+
+for group, seq in seq_group:
+    print(group)
+    # Extract file names
+    forward = seq[seq.direction == 'forward'].filename.values[0]
+    # Define inputs for fastp
+    in1 = f'{INPUT_DIR}{forward}'  # forward input read
+
+    # Define outputs
+    out1 = f'{OUTPUT_DIR}{forward}'  # reverse input read
+    html_report = f'{OUTPUT_DIR}{DATE}_{group}_fastp_report.html'
+    json_report = f'{OUTPUT_DIR}{DATE}_{group}_fastp_report.json'
+    report_title = f'{DATE}{DESCRIPTION} fastp report'
+
+    # Define string to be ran on the terminal
+    fastp = f'''fastp \
+        --in1 {in1} \
+        --out1 {out1} \
+        --verbose \
+        --max_len1 60 \
+        --disable_adapter_trimming \
+        --html {html_report} \
+        --json {json_report} \
+        --report_title "{html_report}" \
+    '''
+
+    # Run program
+    os.system(fastp)
